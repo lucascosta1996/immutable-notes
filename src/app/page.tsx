9e58807ab3a00964a864db28f25d141b0ed74f29
Lucas Costa <lucas.alves.costa96@gmail.com>
2025-07-15 21:15:37 -0300

    Change file names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from "react";
 import ColorPicker from "@/components/ColorPicker/ColorPicker";
-import NftSvgPreview from "@/components/NFTPreview/NFTPreview";
+import NftSvgEditor from "@/components/NftSvgEditor/NftSvgEditor";
 
 export default function Home() {
   const [selectedColor, setSelectedColor] = useState<string | null>('#af84b4');
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <div className="flex gap-4 items-center align-middle flex-col sm:flex-row h-auto sm:h-auto">
-          <NftSvgPreview backgroundColor={selectedColor} />
+          <NftSvgEditor backgroundColor={selectedColor} />
           <ColorPicker onColorSelect={setSelectedColor} />
         </div>
       </main>
